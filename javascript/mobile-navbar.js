a8343c383ad09182c1c936e4e57350e517f4a98e
Rodrigo-Nery <rneribastos@hotmail.com>
2024-12-11 00:56:43 -0300

    inicializações e efeitos globais

--- javascript/mobile-navbar.js
+++ javascript/mobile-navbar.js
@@ -3,3 +3,15 @@ const hamburger = document.querySelector(".header--icon-btn");
 const nav = document.querySelector(".nav--ul");
 
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
+
+window.addEventListener("load", function () {
+    document.body.classList.add("loaded");
+  });
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    AOS.init();
+  });
+  
+  
+  
